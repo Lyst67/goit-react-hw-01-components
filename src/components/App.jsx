b29a959a5f0c682c7726eaebf5a26acf172c2241
Lyst67 <p_listopad@ukr.net>
2023-09-01 23:13:42 +0300

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from "./Profile/Profile";
+import { Section } from "./Section";
+import user from '../user.json'
+
+ export const App = () => {
+   return (
+     <>
+        <Section title='Profile Section'>
+      <Profile username={user.username} tag={user.tag}
+               location={user.location}
+               avatar={user.avatar}
+               stats={user.stats}/>   
+     </Section>
+
+     <Section title='Statistic Section'></Section>
+     </>
+    
+     )}; 
+  
+    //  <Section title='Statistics Section'>
+    //     <Statistics title='Upload stats' stats={data} />
+    //   </Section>
+    //   <Section title='Friends list'>
+    //     <FriendList friends={friends} />
+    //   </Section>
+    //   <Section title='Transaction history'>
+    //     <TransactionHistory items={transactions} />
+    //   </Section>
+ 
+   
+
+ 
---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+
+
+ export const Section = ({children}) => {
+   return ( 
+     <div>
+       {children}
+     </div>
+       )}; 
+     
